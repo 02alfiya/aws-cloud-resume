830348ba32aa2ac7d03239036362451c4412e85c
allfi-06 <alfiyatamboli8@gmail.com>
2026-08-31 08:28:17 +0530

    Updated terraform main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -243,3 +243,70 @@ resource "aws_cloudfront_distribution" "resume_site_CDN" {
     Name = "alfiyajaved.in"
   }
 }
+
+data "tls_certificate" "github_actions" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+
+  
+}
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = [ "sts.amazonaws.com" ]
+  thumbprint_list = [ data.tls_certificate.github_actions.certificates[0].sha1_fingerprint ]
+  
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name = "github-actions-deploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:02alfiya/aws-cloud-resume:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+  
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy_policy" {
+  name = "github-actions-deploy-policy"
+  role = aws_iam_role.github_actions_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["s3:PutObject", "s3:GetObject", "s3:ListBucket","s3:DeleteObject"]
+        Resource = [aws_s3_bucket.resume_site.arn,"${aws_s3_bucket.resume_site.arn}/*"]
+
+      },
+      {
+        Effect = "Allow"
+        Action = "cloudfront:CreateInvalidation"
+        Resource = aws_cloudfront_distribution.resume_site_CDN.arn
+      },
+      {
+        Effect = "Allow"
+        Action = "lambda:UpdateFunctionCode"
+        Resource = aws_lambda_function.lambda_visitor_count.arn
+      }
+    ]
+  })
+  
+}
